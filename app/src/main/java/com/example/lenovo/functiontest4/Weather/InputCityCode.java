30c8574c82741b66c15f3b19ab5b795e5ee38de8
yj9940 <786436456@qq.com>
2018-05-15 14:23:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/functiontest4/Weather/InputCityCode.java
@@ -0,0 +1,15 @@
+package com.example.lenovo.functiontest4.Weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import com.example.lenovo.functiontest4.R;
+
+public class InputCityCode extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.suggest_show);
+    }
+}
